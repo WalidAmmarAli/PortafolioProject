afca8b37ae62e41414f639438e1fe5e54ce7337a
Walid Ammar <ammaralialiwa@gmail.com>
2023-05-23 09:44:13 -0600

    Modify the javascript file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const openMenuButton = document.getElementById('button-bar');
 const closeButton = document.getElementById('mobile-id');
 const menuMobile = document.getElementById('menu-mobile');
+const workDetailLinks = document.querySelectorAll('.options');
 
 openMenuButton.addEventListener('click', openMenu);
 closeButton.addEventListener('click', closeMenu);
+workDetailLinks.forEach(function(link) {
+  link.addEventListener('click', closeMenu);
+});
 
 function openMenu() {
   menuMobile.style.display = 'block';
